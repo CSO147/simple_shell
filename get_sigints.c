efa4a4343f73004a8d0afeb3e5564c4ba887a9ad
SNRAdesh <adesinatoheeb7@gmail.com>
2023-05-24 13:26:19 -0700

    get_sigints.c

---
+++ get_sigints.c
@@ -0,0 +1,12 @@
+/**
+ * get_sigint - func to handle the crtl + c call in prompt
+ * @sig: Signal handler
+ *
+ * Return: void
+ */
+
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
