7fb938065cbae1a002ea0e0588436c67dabe5515
SNRAdesh <adesinatoheeb7@gmail.com>
2023-05-24 14:18:27 -0700

    read_lines.c

---
+++ read_lines.c
@@ -0,0 +1,14 @@
+/**
+ * read_line - func that reads the input strin
+ * @i_eof: return value of getline function
+ * Return: input string
+ */
+char *read_line(int *i_eof)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+
+	*i_eof = getline(&input, &bufsize, stdin);
+
+	return (input);
+}
